df3df9b1248f78a78f8ad744e0ae38a7af1da540
Yasic <2207793765@qq.com>
2015-11-06 00:09:49 +0800

    update MIUITime view

--- app/src/main/java/com/tesmple/chromeprocessbar/Views/MIUITimeView.java
+++ app/src/main/java/com/tesmple/chromeprocessbar/Views/MIUITimeView.java
@@ -1,5 +1,6 @@
 package com.tesmple.chromeprocessbar.Views;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,6 +15,7 @@ import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 import com.tesmple.chromeprocessbar.R;
 
@@ -158,7 +160,7 @@ public class MIUITimeView extends View {
 
     @Override
     protected void onDraw(Canvas canvas){
-        canvas.drawColor(Color.parseColor("#3399ff"));
+        canvas.drawColor(Color.parseColor("#03a9f4"));
         if(animationBeginFlag == 1){
             initParameter();
             startTime();
@@ -167,27 +169,32 @@ public class MIUITimeView extends View {
 
         /*画时针*/
         canvas.save();
-        canvas.rotate(360*hour/12 + 360*minute/(60*12) , centerX, centerY);
+        canvas.rotate(360 * hour / 12 + 360 * minute / (60 * 12), centerX, centerY);
         mPaint.setStrokeWidth(3);
         canvas.drawLine(centerX, centerY - 90, centerX, centerY - 7, mPaint);
         canvas.restore();
 
         /*画分针*/
         canvas.save();
-        canvas.rotate(360 * minute / 60, centerX, centerY);
+        canvas.rotate(360 * minute / 60 + 360 * second / (60 * 60), centerX, centerY);
         mPaint.setStrokeWidth(3);
         canvas.drawLine(centerX, centerY - 131, centerX, centerY - 7, mPaint);
         canvas.restore();
 
-        mPaint.setColor(Color.parseColor("#f7f8f3"));
+        /*画外圆*/
+        mPaint.setColor(Color.parseColor("#80f7f8f3"));
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(centerX, centerY, (radius - 20) * denisty, mPaint);
         canvas.save();
         for(int i = 0;i<360;i = i+30){
-            canvas.drawLine(centerX,centerY-(radius - 25)*denisty,centerX,centerY-(radius - 20)*denisty,mPaint);
+            mPaint.setColor(Color.parseColor("#80f7f8f3"));
+            canvas.drawLine(centerX, centerY - (radius - 25) * denisty, centerX, centerY - (radius - 20) * denisty, mPaint);
             canvas.rotate(30,centerX,centerY);
         }
         canvas.restore();
+        mPaint.setColor(Color.parseColor("#f7f8f3"));
+
+        /*画内圆*/
         mPaint.setStrokeWidth(6);
         canvas.drawCircle(centerX, centerY, 5 * denisty, mPaint);
         mPaint.setStrokeWidth(2);
@@ -218,29 +225,6 @@ public class MIUITimeView extends View {
                 canvas.rotate(6f, centerX, centerY);
             }
         }
-        /*for (int i = 0;i < septalLineNum;i++){
-            if( i > septalLineNum - ROTATIONCOLORARRAY.length){
-                mPaint.setColor(ROTATIONCOLORARRAY[septalLineNum - i][1]);
-                canvas.drawLine(centerX, centerY - (radius + lineLength) * denisty, centerX, centerY - radius * denisty, mPaint);
-                canvas.rotate(2f, centerX, centerY);
-            }else {
-                if(i == 0){
-                    mPaint.setColor(ROTATIONCOLORARRAY[0][1]);
-                    Path path = new Path();
-                    path.moveTo(centerX, centerY - (radius - 3)*denisty);
-                    path.lineTo(centerX - 7, centerY - (radius - 10)*denisty);
-                    path.lineTo(centerX + 7, centerY - (radius - 10)*denisty);
-                    path.close();
-                    canvas.drawPath(path, mPaint);
-                    canvas.drawLine(centerX, centerY - (radius + lineLength) * denisty, centerX, centerY - radius * denisty, mPaint);
-                    canvas.rotate(2f, centerX, centerY);
-                    continue;
-                }
-                mPaint.setColor(ROTATIONCOLORARRAY[ROTATIONCOLORARRAY.length - 1][1]);
-                canvas.drawLine(centerX, centerY - (radius + lineLength) * denisty, centerX, centerY - radius * denisty, mPaint);
-                canvas.rotate(2f, centerX, centerY);
-            }
-        }*/
     }
 
     /**
@@ -256,11 +240,7 @@ public class MIUITimeView extends View {
                 minute = calendar.get(Calendar.MINUTE);
                 second = calendar.get(Calendar.SECOND);
                 millis = System.currentTimeMillis()%1000;
-                /*if(second != septalLineNum){
-                    second = second + 1;
-                }else {
-                    second = 0;
-                }*/
+                //playAnimation();
                 Message message = new Message();
                 message.what = 1 ;
                 handler.sendMessage(message);
@@ -268,6 +248,23 @@ public class MIUITimeView extends View {
         };
         timer.schedule(timerTask,0,1000);
     }
+
+    private void playAnimation(){
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0 , 3 );
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //degreeValue = (Integer)animation.getAnimatedValue();
+                //invalidate();
+            }
+        });
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setRepeatCount(-1);
+        valueAnimator.setRepeatMode(1);
+        valueAnimator.setDuration(animationTime);
+        valueAnimator.start();
+    }
+
 
     /**
      * 初始化参数，包括圆心位置
